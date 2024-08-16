53c78dd55838c4d383ada0390e00acbc63104aec
LikhithaAralimara <128489410+LikhithaAralimara@users.noreply.github.com>
2024-08-17 00:10:43 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,44 @@
+// export const apiKey = "6f26a6235cdb438c91751218230307";
+
+export const apiKey = "a0613fef199440178d5174211241308";
+
+export const weatherImages = {
+    'Partly cloudy': require('../assets/images/partlycloudy.png'),
+    'Moderate rain': require('../assets/images/moderaterain.png'),
+    'Patchy rain possible': require('../assets/images/moderaterain.png'),
+    'Sunny': require('../assets/images/sun.png'),
+    'Clear': require('../assets/images/sun.png'),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Cloudy': require('../assets/images/cloud.png'),
+    'Light rain': require('../assets/images/moderaterain.png'),
+    'Moderate rain at times': require('../assets/images/moderaterain.png'),
+    'Heavy rain': require('../assets/images/heavyrain.png'),
+    'Heavy rain at times': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
+    'Mist': require('../assets/images/mist.png'),
+    'other': require('../assets/images/moderaterain.png')
+}
+// const path = '../assets/images/mist.png';
+
+// export const weatherImages = {
+//   'Partly cloudy': require(path),
+//   'Moderate rain': require(path),
+//   'Patchy rain possible': require(path),
+//   'Sunny': require(path),
+//   'Clear': require(path),
+//   'Overcast': require(path),
+//   'Cloudy': require(path),
+//   'Light rain': require(path),
+//   'Moderate rain at times': require(path),
+//   'Heavy rain': require(path),
+//   'Heavy rain at times': require(path),
+//   'Moderate or heavy freezing rain': require(path),
+//   'Moderate or heavy rain shower': require(path),
+//   'Moderate or heavy rain with thunder': require(path),
+//   'Mist': require(path),
+//   'other': require(path)
+// };
+
+  
